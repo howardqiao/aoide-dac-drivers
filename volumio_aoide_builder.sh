3969ec0f0cac8531caac5cbeace528bef7da240d
Howard Qiao <howardqiao@u-geek.net>
2018-03-13 09:09:26 +0800

    add IR support

--- volumio_aoide_builder.sh
+++ volumio_aoide_builder.sh
@@ -3,6 +3,7 @@ VERSION="2.368"
 DEFAULT_SS="mirrordirector.raspbian.org/raspbian"
 SS="mirrordirector.raspbian.org\/raspbian"
 REPONAME=""
+IR_Support=true
 function download(){
 git clone https://github.com/volumio/build.git --depth 1
 }
@@ -20,10 +21,16 @@ rm -rf ./build
 function aoide_patch(){
 sed -i -e '/BUILD="arm"/r volumio_aoide1.txt' build/build.sh
 sed -i -e '/Cloning Volumio UI/r volumio_aoide2.txt' build/build.sh
+if [ "$IR_Support" = true ]; then
+	sed -i -e '/Writing cmdline.txt file/r volumio_aoide_lirc_support.txt' build/scripts/raspberryconfig.sh
+fi
 }
 function aoide_pitft_patch(){
 sed -i -e '/BUILD="arm"/r volumio_aoide_pitft1.txt' build/build.sh
 sed -i -e '/Cloning Volumio UI/r volumio_aoide_pitft2.txt' build/build.sh
+if [ "$IR_Support" = true ]; then
+	sed -i -e '/Writing cmdline.txt file/r volumio_aoide_lirc_support.txt' build/scripts/raspberryconfig.sh
+fi
 }
 function setsource(){
 sed -i 's/^source.*$/source=http:\/\/'"$SS"'/g' build/recipes/arm.conf
@@ -36,6 +43,11 @@ cd ..
 }
 function gz(){
 gzip build/*.img
+sleep 3
+if [ ! -d "output" ]; then
+mkdir output
+fi
+mv build/*.img.gz output/
 }
 for (( ; ; ))
 do
@@ -46,6 +58,7 @@ OPTION=$(whiptail --title "Volumio Image Build Tools(V$VERSION)" --menu "Choose 
 "Original" "Build original image." \
 "AOIDE" "Build with AOIDE DACs Drivers." \
 "PITFT" "Build with AOIDE DACs Drivers and Screen." \
+"IRSupport" "Build with IR Support." \
 "Version" "Set image version." \
 "Source" "Set apt source of volumio." \
 "Clear" "Clear build folder." \
@@ -54,7 +67,7 @@ OPTION=$(whiptail --title "Volumio Image Build Tools(V$VERSION)" --menu "Choose 
 case $OPTION in
 	"Tools")
 	apt update
-	apt install git squashfs-tools kpartx multistrap qemu-user-static samba debootstrap parted dosfstools qemu binfmt-support qemu-utils
+	apt install git squashfs-tools kpartx multistrap qemu-user-static samba debootstrap parted dosfstools qemu binfmt-support qemu-utils docker.io
 	;;
 	"Download")
 	clear_all
@@ -81,6 +94,24 @@ case $OPTION in
 	REPONAME="aoide_pitft"
 	build
 	gz
+	;;
+	"IRSupport")
+	OPTION_IR=$(whiptail --title "Build with IR Support?" --menu "Choose an option." \
+		--cancel-button "exit" 20 66 11 \
+		"EnableIR" "Enable IR Support" \
+		"DisableIR" "Disable IR Support" \
+		"Exit" "Exit" \
+		3>&1 1>&2 2>&3)
+	case $OPTION_IR in
+		"EnableIR")
+		IR_Support=true
+		;;
+		"DisableIR")
+		IR_Support=false
+		;;
+		"Exit")
+		;;
+	esac
 	;;
 	"Version")
 	VERSION_CUSTOM=$(whiptail --inputbox "What is your image version?" 6 60 $VERSION --title "Volumio image version" 3>&1 1>&2 2>&3)
