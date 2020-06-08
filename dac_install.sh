bbdd5942a167918b82c1228bac58f39570f92f97
Howard Qiao <howard.qiao@u-geek.net>
2020-06-08 23:03:03 +0800

    New GUI for dac install

--- dac_install.sh
+++ dac_install.sh
@@ -1,169 +1,250 @@
 #!/bin/bash
-driver_version="4.19.86"
-firmware_hash="b9ecbe8d0e3177afed08c54fc938938100a0b73f"
+
+# AOIDE DAC Drivers Installer
+
+# Newest driver version and firmware git hash
+TITLE="AOIDE DAC Drivers Installer"
+BACKTITLE="UGEEK WORKSHOP [ ugeek.aliexpress.com | ukonline2000.taobao.com ]"
+driver_version="4.19.118"
+firmware_hash="e1050e94821a70b2e4c72b318d6c6c968552e9a2"
+
 driver_path="/lib/modules/"$driver_version+"/kernel/sound/soc/bcm/aoide-dacii.ko"
 driver_url="https://github.com/howardqiao/aoide-dac-drivers/raw/master/drivers/aoide_dac_"$driver_version".tar.gz"
 driver_filename="aoide_dac_"$driver_version".tar.gz"
-kernel_installed=1
-driver_installed=1
-driver_selected="none"
 dtoverlay=""
-dac_current="none"
+file_config="/boot/config.txt"
+file_config_dietpi="/DietPi/config.txt"
 
-function welcome(){
-	echo "Welcome..."
-}
-function menu_show(){
-	echo ""
-	echo "Aoide driver installer"
-	echo "-----------------------"
-	echo "1) Aoide DAC II"
-	echo "2) Aoide DAC Pro"
-	echo "3) Aoide Digi Pro"
-	echo "4) Aoide Zero DAC+"
-	echo "5) Aoide Zero Digi+"
-	echo ""
-	echo "5) Exit"
-	echo "-----------------------"
-	echo "Enabled DAC:"$dac_current
-	echo "-----------------------"
-}
-function kernel_check(){
-	echo "Driver Version:"
-	echo $driver_version
-	driver_version_length=${#driver_version}
+# Get kernel version
+function get_kernel_version(){
 	kernel_version=$(uname -r)
-	echo "Current Kernel Version:"
-	echo $kernel_version
-	kernel_compare=${kernel_version:0:$driver_version_length}
-	# echo "Kernel Compare:"
-	# echo $kernel_compare
-	if [ "$driver_version" = "$kernel_compare" ]; then
-		return 0
-	else
-		return 1
-	fi
+	IFS='-' read -ra kernel_version <<< "$kernel_version"
 }
+
+# Install newest kernel
 function kernel_install(){
 	echo "Install Raspberry PI Kernel "$driver_version
-	apt update
-	apt install binutils curl
-	curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
+	if [ ! -f /usr/bin/rpi-update ]; then
+		apt update
+		apt install binutils curl
+		curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
+	fi
 	UPDATE_SELF=0 SKIP_BACKUP=1 rpi-update $firmware_hash
 }
-function driver_check(){
-	echo $driver_path
-	if [ -f "$driver_path" ]; then
-		return 0
-	else
-		return 1
-	fi
-}
-function driver_install(){
-	echo "Install Aoide DACs driver V"$driver_version
-	cd /
-	if [ -f "$driver_filename" ]; then
-		rm $driver_filename
-	fi
-	#wget https://github.com/howardqiao/aoide-dac-drivers/raw/master/drivers/aoide_dac_$driver_version.tar.gz
-	echo "Download driver($driver_version) now..."
-	echo "URL:"$driver_url
-	echo "FILENAME:"$driver_filename
-	curl -L $driver_url -o $driver_filename --progress --retry 10 --retry-delay 10  --retry-max-time 100
-	#wget $driver_url 
-	if [ -f "$driver_filename" ]; then
-		tar zxvf $driver_filename
-		rm $driver_filename
-		depmod -b / -a $driver_version+
-		depmod -b / -a $driver_version-v7+
-		depmod -b / -a $driver_version-v7l+
-		depmod -b / -a $driver_version-v8+
-		sync
-	else
-		echo "Download driver failed"
-	fi
-}
-function driver_disable(){
-	sed -i "s/audio=on/audio=off/" /boot/config.txt
-	sed -i '/dtoverlay=aoide-dacii/d' /boot/config.txt
-	sed -i '/dtoverlay=aoide-dacpro/d' /boot/config.txt
-	sed -i '/dtoverlay=aoide-digipro/d' /boot/config.txt
-	sed -i '/dtoverlay=aoide-zero-dacplus/d' /boot/config.txt
-	sed -i '/dtoverlay=aoide-zero-digiplus/d' /boot/config.txt
-	sed -i "s/audio=on/audio=off/" /DietPi/config.txt
-	sed -i '/dtoverlay=aoide-dacii/d' /DietPi/config.txt
-	sed -i '/dtoverlay=aoide-digipro/d' /DietPi/config.txt
-	sed -i '/dtoverlay=aoide-zero-dacplus/d' /DietPi/config.txt
-	sed -i '/dtoverlay=aoide-zero-digiplus/d' /DietPi/config.txt
-}
-function driver_enable(){
 
-	kernel_check
-	if [ $? -eq 1 ]; then
-		kernel_install
-	else
-		echo "Kernel OK!"
+# Test function
+# function driver_install(){
+	# echo "Install Aoide DACs driver V"$driver_version
+	# cd /
+	# if [ -f "$driver_filename" ]; then
+		# rm $driver_filename
+	# fi
+	# #wget https://github.com/howardqiao/aoide-dac-drivers/raw/master/drivers/aoide_dac_$driver_version.tar.gz
+	# echo "Download driver($driver_version) now..."
+	# echo "URL:"$driver_url
+	# echo "FILENAME:"$driver_filename
+	# curl -L $driver_url -o $driver_filename --progress --retry 10 --retry-delay 10  --retry-max-time 100
+	# #wget $driver_url 
+	# if [ -f "$driver_filename" ]; then
+		# tar zxvf $driver_filename
+		# rm $driver_filename
+		# depmod -b / -a $driver_version+
+		# depmod -b / -a $driver_version-v7+
+		# depmod -b / -a $driver_version-v7l+
+		# depmod -b / -a $driver_version-v8+
+		# sync
+	# else
+		# echo "Download driver failed"
+	# fi
+# }
+
+# Disable DAC in config.txt
+function disable_dac(){
+	sed -i "s/audio=on/audio=off/" $file_config
+	sed -i '/dtoverlay=aoide-dacii/d' $file_config
+	sed -i '/dtoverlay=aoide-dacpro/d' $file_config
+	sed -i '/dtoverlay=aoide-digipro/d' $file_config
+	sed -i '/dtoverlay=aoide-zero-dacplus/d' $file_config
+	sed -i '/dtoverlay=aoide-zero-digiplus/d' $file_config
+	sed -i '/dtoverlay=aoide-zpod-dac/d' $file_config
+	sed -i '/dtoverlay=raspivoicehat/d' $file_config
+	
+	if [ -f "$file_config_dietpi" ]; then
+		sed -i "s/audio=on/audio=off/" $file_config_dietpi
+		sed -i '/dtoverlay=aoide-dacii/d' $file_config_dietpi
+		sed -i '/dtoverlay=aoide-dacpro/d' $file_config_dietpi
+		sed -i '/dtoverlay=aoide-digipro/d' $file_config_dietpi
+		sed -i '/dtoverlay=aoide-zero-dacplus/d' $file_config_dietpi
+		sed -i '/dtoverlay=aoide-zero-digiplus/d' $file_config_dietpi
+		sed -i '/dtoverlay=aoide-zpod-dac/d' $file_config_dietpi
+		sed -i '/dtoverlay=raspivoicehat/d' $file_config_dietpi
 	fi
-	driver_check
-	if [ $? -eq 1 ]; then
-		driver_install
-	else
-		echo "Driver OK!"
-	fi
-	driver_disable
-	echo "dtoverlay="$dtoverlay >> /boot/config.txt
-	echo "dtoverlay="$dtoverlay >> /DietPi/config.txt
-	read -p "You should reboot to enable the selected DAC(y/n)" b
-	case $b in 
-	y)
-	reboot
-	;;
-	*)
-	exit 1
-	;;
-	esac
 }
+
+# Install newest kernel and driver
+function install_newest_kernel_driver(){
+	kernel_install
+	if [ -f "drivers/aoide_volumio_$driver_version.tar.gz" ]; then
+		cp drivers/aoide_volumio_$driver_version.tar.gz /
+		cd /
+		tar zxvf drivers/aoide_volumio_$driver_version.tar.gz
+		rm drivers/aoide_volumio_$driver_version.tar.gz
+	fi
+	if [ -f "drivers/aoide_dac_$driver_version.tar.gz" ]; then
+		cp drivers/aoide_dac_$driver_version.tar.gz /
+		cd /
+		tar zxvf drivers/aoide_dac_$driver_version.tar.gz
+		rm drivers/aoide_dac_$driver_version.tar.gz
+	fi
+	cd ~
+}
+
+# Deploy driver
+function deploy_driver(){
+	depmod -b / -a $driver_version+
+	depmod -b / -a $driver_version-v7+
+	depmod -b / -a $driver_version-v7l+
+	depmod -b / -a $driver_version-v8+
+}
+
+# Enable dac in config.txt
+function enable_dac_in_config(){
+	# Disable all DAC first
+	disable_dac
+	
+	# Enable dac in config.txt
+	echo "dtoverlay="$dtoverlay >> /boot/config.txt
+	if [ -f "$file_config_dietpi" ]; then
+		echo "dtoverlay="$dtoverlay >> $file_config_dietpi
+	fi
+}
+
+# Reboot prompt
+function reboot_prompt(){
+	if [ -z "$1" ]; then
+		return
+	fi
+	if (whiptail --title "$TITLE" \
+	--backtitle "$BACKTITLE" \
+	--yes-button "Reboot" --no-button "NO" \
+	--yesno "Reboot system to enable driver" 10 60) then
+		sync
+		reboot
+	else
+		return
+	fi
+}
+
+# Enable DAC
+function enable_dac(){
+	# If there's driver pair current version of kernel
+	if [ -f "/boot/overlays/$dtoverlay.dtbo" ]; then
+		enable_dac_in_config
+		reboot_prompt
+		return
+	fi
+	if [ -f "drivers/aoide_dac_$kernel_version.tar.gz" ]; then
+		cp drivers/aoide_dac_$kernel_version.tar.gz /
+		cd /
+		tar zxvf aoide_dac_$kernel_version.tar.gz
+		rm aoide_dac_$kernel_version.tar.gz
+	fi
+	if [ -f "drivers/aoide_volumio_$kernel_version.tar.gz" ]; then
+		cp drivers/aoide_volumio_$kernel_version.tar.gz /
+		cd /
+		tar zxvf aoide_volumio_$kernel_version.tar.gz
+		rm aoide_volumio_$kernel_version.tar.gz
+	fi
+	
+	# If there's no specfied dtbo file , Install newest kernel and driver
+	if [ ! -f "/boot/$dtoverlay.dtbo" ]; then
+		install_newest_kernel_driver
+	fi
+	
+	# Deploy driver
+	deploy_driver
+	
+	# Enable dac in config.txt
+	enable_dac_in_config
+	
+	# reboot_prompt
+	if [ -z "$1" ]; then
+		reboot_prompt
+	fi
+}
+
+# Check privileges
 if [ $UID -ne 0 ]; then
     echo "Superuser privileges are required to run this script."
     echo "e.g. \"sudo $0\""
     exit 1
 fi
-welcome
+
+# Get kernel version
+get_kernel_version
+
+# Check para
+if [ ! -z "$1" ]; then
+	echo "Install driver of $1"
+	dtoverlay=$1
+	enable_dac
+	exit 1
+fi
+
+# Main loop
 while true
 do
-	menu_show
-	read -p "Select your DAC:" driver_selected
-
-	case $driver_selected in 
-	1)
-	echo "You selected Aoide DAC II"
-	dtoverlay="aoide-dacii"
-	driver_enable
-	;;
-	2)
-	echo "You selected Aoide DAC Pro"
-	dtoverlay="aoide-dacpro"
-	driver_enable
-	;;
-	3)
-	echo "You selected Aoide Digi Pro"
-	dtoverlay="aoide-digipro"
-	driver_enable
-	;;
-	4)
-	echo "You selected Aoide Zero DAC+"
-	dtoverlay="aoide-zero-dacplus"
-	driver_enable
-	;;
-	5)
-	echo "You selected Aoide Zero Digi+"
-	dtoverlay="aoide-zero-digiplus"
-	driver_enable
-	;;
-	6)
-	exit 1
-	;;
+	OPTION=$(whiptail --title "$TITLE(V$driver_version)" \
+	--menu "Select your DAC(Sound Card)." \
+	--backtitle "$BACKTITLE" \
+	--cancel-button "Exit" 17 60 9 \
+	"1" "AOIDE DAC Pro" \
+	"2" "AOIDE DAC II" \
+	"3" "AOIDE Digi Pro" \
+	"4" "AOIDE Zero DAC+" \
+	"5" "AOIDE ZPOD DAC" \
+	"6" "Raspi Voice HAT" \
+	"D" "Disable DAC" \
+	"E" "Exit" \
+	3>&1 1>&2 2>&3)
+	case $OPTION in
+		1)
+		dtoverlay="aoide-dacpro"
+		enable_dac
+		;;
+		2)
+		dtoverlay="aoide-dacii"
+		enable_dac
+		;;
+		3)
+		dtoverlay="aoide-digipro"
+		enable_dac
+		;;
+		4)
+		dtoverlay="aoide-zero-dacplus"
+		enable_dac
+		;;
+		5)
+		dtoverlay="aoide-zero-digiplus"
+		enable_dac
+		;;
+		6)
+		dtoverlay="aoide-zpod-dac"
+		enable_dac
+		;;
+		7)
+		dtoverlay="raspivoicehat"
+		enable_dac
+		;;
+		8)
+		install_newest_kernel_driver
+		;;
+		"D")
+		disable_dac
+		;;
+		"E")
+		exit 1
+		;;
 	esac
-	
 done
 
